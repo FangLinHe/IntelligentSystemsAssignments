a2b137635649a12e70ecdfdf02a50f4206f0b841
FangLinHe <FangLinHe@users.noreply.github.com>
2016-06-01 11:19:53 +0800

    Remove unnecessary code & add comments

--- Assignment_4/get_P.m
+++ Assignment_4/get_P.m
@@ -29,35 +29,3 @@ for i = 1:size(maze, 1)
         end
     end
 end
-%{
-S.x = 1:size(maze, 2);
-S.y = 1:size(maze, 1);
-for i = S.y
-    for j = S.x
-        if maze(i, j) % value = 1: not wall
-            for a = 1:4 % expected action
-                for ra = 1:4 % real action
-                    nst.y = i + A(ra, 1);
-                    nst.x = j + A(ra, 2);
-                    if nst.y == 0
-                        nst.y = max(S.y);
-                    elseif nst.y == max(S.y)
-                        nst.y = 1;
-                    end
-                    if nst.x == 0
-                        nst.x = max(S.x);
-                    elseif nst.y == max(S.x)
-                        nst.x = 1;
-                    end
-                    prob = 0.7 * (a == ra) + 0.1 * (a ~= ra);
-                    if maze(nst.y, nst.x) % value = 1: not wall
-                        P(i, j, a, nst.y, nst.x) = P(i, j, a, nst.y, nst.x) + prob;
-                    else
-                        P(i, j, a, i, j) = P(i, j, a, i, j) + prob;
-                    end
-                end
-            end
-        end
-    end
-end
-%}
