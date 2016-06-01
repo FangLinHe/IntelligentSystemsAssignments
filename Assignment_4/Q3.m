61881310edb17ef37b4c1c738a946b7d2338323f
FangLinHe <FangLinHe@users.noreply.github.com>
2016-06-01 10:59:36 +0800

    Remove unnecessary code & add comments

--- Assignment_4/Q3.m
+++ Assignment_4/Q3.m
@@ -1,13 +1,14 @@
 close all;
 clear all;
 
+% Load maze, rewards, status, actions
 maze = importdata('Q3_map.txt', ' '); % 0: wall, 1: road, 2: start, 3: goal
 R = importdata('Q3_reward.txt', ' ');
 if size(maze) ~= size(R)
     error('Size of maze and rewards should be the same.');
 end
-%pi = zeros(size(maze, 1), size(maze, 2), 4) + 0.25; % pi(s, a) = 1/4 for all s, a
 S = get_S(maze);
+% actions: moving like a knight
 A = [ 1,  2;
       1, -2;
      -1,  2;
@@ -16,9 +17,8 @@ A = [ 1,  2;
       2, -1;
      -2,  1;
      -2, -1];
-      
-%P = get_P(maze, S, A);
-%para.stop_thres = 0.001; % stop threshold
+
+% parameters
 para.discount_factor = 0.9; % discount factor
 para.learning_rate = 0.4;
 para.max_episode = 100;
@@ -27,6 +27,6 @@ para.epsilon = 0.3;
 para.prob_follow_act = 0.72;
 para.path_filename = 'Q3_path.txt';
 
-
+% Q learning & plot
 [Q, acum_reward] = Q_learning(maze, A, R, para);
 plot(acum_reward)
